3a4763ddd9b1bfe4a63d711a34d06d93a49096cc
rios-matias <rios.matias998@gmail.com>
2024-06-16 22:52:46 -0300

    feat: product card improvements

--- src/app/ui/admin/productCard.tsx
+++ src/app/ui/admin/productCard.tsx
@@ -1,5 +1,8 @@
+'use client'
 import React from 'react';
-import { Product } from '../lib/Entities/Product';
+import { Product } from '@/app/lib/Entities/Product';
+import DeleteButton from './deleteButton';
+import { PHASE_PRODUCTION_BUILD } from 'next/dist/shared/lib/constants';
 
 const ProductCard = ({ product }:{product:Product}) => {
   return (
@@ -8,17 +11,21 @@ const ProductCard = ({ product }:{product:Product}) => {
       <div className="px-6 py-4 flex-1 flex flex-col justify-between">
         <div>
           <div className="font-bold text-xl mb-2">{product.productname}</div>
-          <p className="text-gray-700 text-base mb-2">${product.price}</p>
+          <div className="flex justify-between items-center mb-2">
+            <p className="text-gray-700 text-base">${product.price}</p>
+            <p className="text-gray-700 text-base">Stock: {product.stock}</p>
+          </div>
         </div>
-        <div>
+        <div className="flex space-x-2">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
             Modificar
           </button>
+          
+          <DeleteButton data={{id:product.id, imageId:product.imagekey}}></DeleteButton>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default ProductCard;
