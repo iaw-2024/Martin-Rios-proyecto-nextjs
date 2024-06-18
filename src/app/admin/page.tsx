18e3f3f3f15125f76a0be9ce4a396aa3157efd4f
rios-matias <rios.matias998@gmail.com>
2024-06-18 20:48:15 -0300

    feat: pagination added to admin main page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,60 @@
 import ProductsRepository from "../lib/Repositories/ProductsRepository";
 import ProductCard from "../ui/admin/productCard";
 import { unstable_noStore as noStore } from 'next/cache';
-export default async function LoginPage() {
+import SearchBar from "../ui/admin/searchBar";
+import { Product } from "../lib/Entities/Product";
+import Pagination from "../ui/admin/Pagination";
+import { Fragment } from "react";
+
+export default async function LoginPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
   noStore();
+
+  const ITEMS_PER_PAGE= 3
+
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
   const productsRepository = new ProductsRepository();
-  const products = await productsRepository.getAllProducts();
-  
+  let products: Product[] = [];
+  let totalPages = 1;
+
+  if (query.length > 0) {
+    const result = await productsRepository.searchProductsByName(query, currentPage, ITEMS_PER_PAGE);
+    products = result.products;
+    totalPages = Math.ceil(result.total / ITEMS_PER_PAGE)
+  } else {
+    const result = await productsRepository.getAllProductsPaginated(currentPage, ITEMS_PER_PAGE);
+    products = result.products;
+    totalPages = Math.ceil(result.total / ITEMS_PER_PAGE)
+  }
+
   return (
-    <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+    <Fragment>
+      <div className="container mx-auto px-4">
+        <SearchBar />
+        {products.length === 0 ? (
+          <div className="h-screen flex items-center justify-center bg-gray-100">
+          <div className="text-center text-gray-700">
+              <p className="text-xl">parece que no encontramos resultados para su búsqueda..</p>
+          </div>
       </div>
-    </div>
+        ) : (
+          <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+              {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </div>
+            <Pagination totalPages={totalPages} currentPage={currentPage} query={query} />
+          </div>
+        )}
+      </div>
+    </Fragment>
   );
 }
